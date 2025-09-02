d88ed26426df3a0ba74978bf0a6a4df0d855f99f
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2025-09-01 22:36:07 -0300

    usuario novo

--- users_validation.js
+++ users_validation.js
@@ -1,6 +1,6 @@
 [
   { "expirationDate": "31/12/2025", "userName": "NARNIA 3" }, 
-  { "expirationDate": "17/07/2025", "userName": "CORVA1" },
+  { "expirationDate": "31/12/2025", "userName": "CORVA1" },
   { "expirationDate": "17/07/2025", "userName": "- The Clown -" },
   { "expirationDate": "31/12/2025", "userName": "P I Z A C" },
   { "expirationDate": "31/12/2025", "userName": "Tinna" },
