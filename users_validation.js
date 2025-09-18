1e2877d327937f0e989e3ef38bcc05815ef04f1f
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2025-09-18 20:19:59 -0300

    correcao user

--- users_validation.js
+++ users_validation.js
@@ -14,5 +14,6 @@
   { "expirationDate": "31/12/2025", "userName": "Sayb0t" },
   { "expirationDate": "31/12/2025", "userName": "FatalFight" },
   { "expirationDate": "31/12/2025", "userName": "SERGINHO1234" },
-  { "expirationDate": "31/12/2025", "userName": "Shady HyyK" }
+  { "expirationDate": "31/12/2025", "userName": "Shady HyyK" },
+  { "expirationDate": "31/12/2025", "userName": "Teste" }
 ]
